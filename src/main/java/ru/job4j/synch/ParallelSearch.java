f4ead823b5ac4df39ba997b4452292b79acc568c
MikhailChrn <mikhail.cherneyev@yandex.ru>
2024-09-03 10:49:50 +0400

    2. Обеспечить остановку потребителя. [#66825]

--- src/main/java/ru/job4j/synch/ParallelSearch.java
+++ src/main/java/ru/job4j/synch/ParallelSearch.java
@@ -1,24 +1,11 @@
 package ru.job4j.synch;
 
-/**
- * Одна нить ищет файлы с подходящим именем.
- * Вторая нить берет эти файлы и читает.
- * <p>
- * Эта схема хорошо описывается шаблоном Producer-Consumer.
- * Однако есть один момент.
- * <p>
- * Когда первая нить заканчивает свою работу, потребители переходят в режим wait.
- * <p>
- * Изменить код, так, что бы потребитель завершал свою работу.
- * (Код нужно изменить в методе main.)
- */
-
 public class ParallelSearch {
     public static void main(String[] args) {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(10);
         final Thread consumer = new Thread(
                 () -> {
-                    while (true) {
+                    while (!Thread.currentThread().isInterrupted()) {
                         try {
                             System.out.println(queue.poll());
                         } catch (InterruptedException e) {
@@ -28,7 +15,9 @@ public class ParallelSearch {
                     }
                 }
         );
+
         consumer.start();
+
         new Thread(
                 () -> {
                     for (int index = 0; index != 3; index++) {
@@ -47,5 +36,13 @@ public class ParallelSearch {
                 }
 
         ).start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        consumer.interrupt();
     }
 }
